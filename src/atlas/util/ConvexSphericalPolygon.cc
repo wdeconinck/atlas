e6f8cc27e8acbd11d60ed4b697630f6d50e98aef
Slavko Brdar <slavko.brdar@ecmwf.int>
2021-01-14 15:30:40 +0000

    another fix for higher resolution: increase search radius by 1.3x

--- src/atlas/util/ConvexSphericalPolygon.cc
+++ src/atlas/util/ConvexSphericalPolygon.cc
@@ -40,8 +40,8 @@ bool approx_eq_null( const PointXYZ& v1, const double t = ConvexSphericalPolygon
 }
 
 double cart_diff( const PointXYZ& v1, const PointXYZ& v2 ) {
-    double m12 = std::max( std::abs( v1[0] - v2[0] ), std::abs( v1[1] - v2[1] ) );
-    return std::max( m12, std::abs( v1[2] - v2[2] ) );
+    PointXYZ v12 = v1 - v2;
+    return PointXYZ::norm( v12 );
 }
 
 PointLonLat sph_to_lonlat( const PointXYZ& p ) {
@@ -84,7 +84,7 @@ ConvexSphericalPolygon::ConvexSphericalPolygon( const std::vector<PointLonLat>& 
         compute_area();
         bbdiam_ = 0.;
         for ( size_t i = 0; i < size_ - 1; ++i ) {
-            bbdiam_ = 1.2 * std::max( bbdiam_, cart_diff( sph_coords_[i], centroid_ ) );
+            bbdiam_ = std::max( bbdiam_, cart_diff( sph_coords_[i], centroid_ ) );
         }
     }
 }
@@ -292,7 +292,7 @@ int ConvexSphericalPolygon::intersect( const PointXYZ& s1, const PointXYZ& s2, P
 // @param[out] intersecting polygon
 ConvexSphericalPolygon ConvexSphericalPolygon::intersect( const ConvexSphericalPolygon& plg ) const {
     std::vector<PointXYZ> iplg_p;
-    if ( cart_diff( plg.centroid_, centroid_ ) > plg.bbdiam_ + bbdiam_ ) {
+    if ( cart_diff( plg.centroid_, centroid_ ) > 1.3 * ( plg.bbdiam_ + bbdiam_ ) ) {
         return ConvexSphericalPolygon();
     }
 
