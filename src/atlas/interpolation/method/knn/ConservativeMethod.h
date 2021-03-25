06b72842644123cc89fb34f9f1199bb8ebe3add9
Slavko Brdar <slavko.brdar@ecmwf.int>
2021-03-25 14:39:21 +0000

    remove warning

--- src/atlas/interpolation/method/knn/ConservativeMethod.h
+++ src/atlas/interpolation/method/knn/ConservativeMethod.h
@@ -38,6 +38,7 @@ public:
         std::ostream& print( std::ostream& os ) {
             os << "centroids         : " << centroids << "\n"
                << "area              : " << weights << "\n";
+			return os;
         }
     };
 
