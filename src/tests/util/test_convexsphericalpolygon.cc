1a541e74a3119e051c732ac5fb63ef6138bc5dc9
Slavko Brdar <slavko.brdar@ecmwf.int>
2022-03-15 13:50:51 +0000

    add unit test for polygon intersection problem from CS-LFR-16 -> O32

--- src/tests/util/test_convexsphericalpolygon.cc
+++ src/tests/util/test_convexsphericalpolygon.cc
@@ -250,6 +250,22 @@ CASE("source_covered") {
 
 CASE("edge cases") {
     Log::info().precision(20);
+    SECTION("CS-LFR-16 -> O32 problem polygon intersection") {
+        const auto plg0 = make_polygon({{174.3750000000001,-16.79832945594544},
+                                        {174.3750000000001,-11.19720014633353},
+                                        {168.7500000000000,-11.03919441545243},
+                                        {168.7500000000000,-16.56868711281520}});
+        const auto plg1 = make_polygon({{174.3750000000000,-12.55775611523100},
+                                        {174.1935483870968,-15.34836475949100},
+                                        {177.0967741935484,-15.34836475949100}});
+        auto iplg       = plg0.intersect(plg1);
+        auto jplg       = plg1.intersect(plg0);
+        EXPECT_APPROX_EQ(iplg.area(), jplg.area(), 2e-15); // can not take 1e-15
+        EXPECT_EQ(iplg.size(), 3);
+        EXPECT_EQ(jplg.size(), 3);
+        EXPECT(iplg.equals(jplg, 1.e-12));
+        Log::info() << "Intersection area difference: " << std::abs(iplg.area() - jplg.area()) << "\n";
+    }
     SECTION("CS-LFR-2 -> O48 problem polygon intersection") {
         const auto plg0 = make_polygon({{180,-45},{180,0},{135,0},{135,-35.26438968}});
         const auto plg1 = make_polygon({{180,-23.31573073},{180,-25.18098558},{-177.75,-23.31573073}});
@@ -264,11 +280,11 @@ CASE("edge cases") {
     SECTION("H128->H256 problem polygon intersection") {
         const auto plg0 = make_polygon({{42.45283019, 50.48004426},
                                         {43.33333333, 49.70239033},
-                                        {44.1509434, 50.48004426},
-                                        {43.26923077, 51.2558069}});
+                                        {44.15094340, 50.48004426},
+                                        {43.26923077, 51.25580690}});
         const auto plg1 = make_polygon({{43.30188679, 50.48004426},
-                                        {43.73831776, 50.0914568},
-                                        {44.1509434, 50.48004426},
+                                        {43.73831776, 50.09145680},
+                                        {44.15094340, 50.48004426},
                                         {43.71428571, 50.86815893}});
         auto iplg       = plg0.intersect(plg1);
         auto jplg       = plg1.intersect(plg0);
