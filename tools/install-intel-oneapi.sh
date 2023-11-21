cf55a93d3969fe195dde0377b384b7e5406a745f
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-11-21 09:30:37 +0000

    hpckit

--- tools/install-intel-oneapi.sh
+++ tools/install-intel-oneapi.sh
@@ -6,9 +6,11 @@ sudo apt-key add $KEY
 rm $KEY
 echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 sudo apt-get update
-sudo apt-get install \
-    intel-oneapi-compiler-fortran \
-    intel-oneapi-compiler-dpcpp-cpp-and-cpp-classic \
-    intel-oneapi-mpi \
-    intel-oneapi-mpi-devel \
-    intel-oneapi-mkl
+#sudo apt-get install \
+#    intel-oneapi-compiler-fortran \
+#    intel-oneapi-compiler-dpcpp-cpp-and-cpp-classic \
+#    intel-oneapi-mpi \
+#    intel-oneapi-mpi-devel \
+#    intel-oneapi-mkl
+
+sudo apt-get isntall intel-basekit intel-hpckit
